5861cc6cd7a7286bec979b3df634183db35d2c83
zhenwei.zzw <zhenwei.zzw@alibaba-inc.com>
2015-10-17 09:41:16 +0800

    modify setup

--- setup.sh
+++ setup.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
 echo "安装将花费一定时间，请耐心等待直到安装完成^_^"
-if which apt-get >/dev/null; then
-	sudo apt-get install -y ctags 
-elif which yum >/dev/null; then
-	sudo yum install -y ctags 
-fi
 
-##Add HomeBrew support on  Mac OS
-if which brew >/dev/null;then
+if which apt-get >/dev/null; then
+	sudo apt-get install -y ctags ack
+elif which yum >/dev/null; then
+	sudo yum install -y ctags ack
+elif which brew >/dev/null;then
     echo "You are using HomeBrew tool"
-    brew install ctags 
-    ##Fix twisted installation Error in Mac caused by Xcode Version limit
-    sudo ARCHFLAGS=-Wno-error=unused-command-line-argument-hard-error-in-future easy_install twisted
+    brew install ctags ack
 fi
 
 sudo ln -s /usr/bin/ctags /usr/local/bin/ctags
@@ -22,7 +18,7 @@ mv -f ~/.vimrc ~/.vimrc_old
 
 #ivim config
 cd ~/ && git clone https://github.com/lostedleo/ivim.git ~/.vim
-mv -f ~/.vim/vimrc ~/.vimrc
+ln -sf ~/.vim/vimrc ~/.vimrc
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 
 tmp_file=ivim_temp
