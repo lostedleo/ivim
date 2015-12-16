5bb2395bcd0b5b05b11a01552eb48817f0aa9195
zhuzhenwei <zhuzhenwei@h73.(none)>
2015-12-16 10:36:45 +0800

    modify cscope

--- makecscope.sh
+++ makecscope.sh
@@ -2,8 +2,8 @@
 
 usage()
 {
-    echo "usage : makecscope src_path project_name"
-    echo "I will create cscope db in ~/cscope/project_name"
+    echo "usage : makecscope src_path project_name [project current path/common path]"
+    echo "I will create cscope db in project current path or ~/cscope/project_name"
 }
 if [ $# -ne 2 ]
 then 
@@ -13,8 +13,14 @@ fi
 
 SRC_PATH=$1
 CSCOPE_PATH=~/cscope/$2
+COMMON_PATH=$3
 
-mkdir -p $CSCOPE_PATH
-cd $CSCOPE_PATH
+if [ xyz$3 != xyz ]; then
+    mkdir -p $CSCOPE_PATH
+    cd $CSCOPE_PATH
+else
+    cd $SRC_PATH
+fi
+
 find $SRC_PATH -name "*.h" -o -name "*.c" -o -name "*.cpp" -o -name "Makefile" -o -name "makefile" > cscope.files
 cscope -Rbqk -i ./cscope.files
