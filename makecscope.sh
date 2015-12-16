fe5ff0401298c0b02c44080eae3576a785e257f8
zhenwei.zzw <zhenwei.zzw@alibaba-inc.com>
2015-12-16 10:38:59 +0800

    modify makecscope

--- makecscope.sh
+++ makecscope.sh
@@ -13,8 +13,14 @@ fi
 
 SRC_PATH=$1
 CSCOPE_PATH=~/cscope/$2
+COMMON_PATH=$3
 
-mkdir -p $CSCOPE_PATH
-cd $CSCOPE_PATH
+if [ xyz$3 != xyz ]; then
+    mkdir -p $CSCOPE_PATH
+    cd $CSCOPE_PATH
+else
+    cd $SRC_PATH
+fi
+
 find $SRC_PATH -name "*.h" -o -name "*.c" -o -name "*.cpp" -o -name "Makefile" -o -name "makefile" > cscope.files
 cscope -Rbqk -i ./cscope.files
