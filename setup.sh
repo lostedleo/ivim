c6212eda4fd6faa7ca736f999538686e620203b8
lostedleo <losted.leo@gmail.com>
2021-01-04 12:42:25 +0800

    modify setup script to add ag and autojump

--- setup.sh
+++ setup.sh
@@ -9,6 +9,7 @@ function set_shell()
   }
 
   command="source ~/.vim/alias"
+  command1="[[ -s /Users/zhuzhenwei/.autojump/etc/profile.d/autojump.sh ]] && source /Users/zhuzhenwei/.autojump/etc/profile.d/autojump.sh"
   shell_name=~/.bashrc
   if [ -f ~/.zshrc ]; then
     shell_name=~/.zshrc
@@ -16,11 +17,13 @@ function set_shell()
     check_alias $shell_name >/dev/null 2>&1
     if [ $? -ne 0 ];then
       echo $command| tee -a $shell_name
+      echo $command1| tee -a $shell_name
     fi
   else
     check_alias $shell_name >/dev/null 2>&1
     if [ $? -ne 0 ];then
       sed -i "N;2a${command}" $shell_name
+      sed -i "N;2a${command1}" $shell_name
     fi
   fi
 }
@@ -30,11 +33,11 @@ function set_shell()
 #
 
 if which apt-get >/dev/null 2>&1; then
-  sudo apt-get install -y ctags ack cscope
+  sudo apt-get install -y ctags ack cscope silversearcher-ag
 elif which yum >/dev/null 2>&1; then
-  sudo yum install -y ctags ack cscope
+  sudo yum install -y ctags ack cscope the_silver_searcher
 elif which brew >/dev/null 2>&1;then
-  brew install ctags ack cscope tmux
+  brew install ctags ack cscope tmux the_silver_searcher
 fi
 
 if [ ! -f /usr/local/bin/ctags ];then
@@ -56,23 +59,25 @@ if [ ! -f ~/.vim/setup.sh ];then
   cd ~/ && git clone https://github.com/lostedleo/ivim.git ~/.vim
 fi
 
-#set shell config
-set_shell
-
+#install efficient tools
 #install ohmyzsh
 sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
 #install fzf
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
+#install autojump
+git clone https://github.com/wting/autojump.git ~/.temp/autojump
+~/.temp/autojump/install.py
 
+#set shell config
+set_shell
 #set vim config
 ln -sf ~/.vim/vimrc ~/.vimrc
 ln -sf ~/.vim/screenrc ~/.screenrc
 ln -sf ~/.vim/tmux.conf ~/.tmux.conf
 ln -sf ~/.vim/config ~/.ssh/config
 
-#
+#install all vim plugins
 if [ ! -d ~/.vim/bundle ];then
   git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 
