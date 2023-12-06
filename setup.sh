a662c9b6e0649069caeea48d694188ef3bf02eb4
zhuzhenwei <losted.leo@gmail.com>
2023-12-06 11:02:39 +0800

    feat:modify setup.sh to auto install ivim

--- setup.sh
+++ setup.sh
@@ -1,20 +1,92 @@
 #!/bin/bash
 # sh -c "$(wget -O- https://raw.githubusercontent.com/lostedleo/ivim/master/setup.sh)"
+set -e
 echo "ivim install vim plugins will task a few minutes, please waiting! ^_^"
 
-function set_shell() {
-  function check_alias() {
-    grep "~/.vim/alias" $1
+if [ -t 1 ]; then
+  is_tty() {
+    true
   }
+else
+  is_tty() {
+    false
+  }
+fi
 
-  function set_command() {
-    cat >> $1 <<- EOF
+# Adapted from code and information by Anton Kochkov (@XVilka)
+# Source: https://gist.github.com/XVilka/8346728
+supports_truecolor() {
+  case "$COLORTERM" in
+  truecolor|24bit) return 0 ;;
+  esac
+
+  case "$TERM" in
+  iterm           |\
+  tmux-truecolor  |\
+  linux-truecolor |\
+  xterm-truecolor |\
+  screen-truecolor) return 0 ;;
+  esac
+
+  return 1
+}
+
+setup_color() {
+  # Only use colors if connected to a terminal
+  if ! is_tty; then
+    FMT_RAINBOW=""
+    FMT_RED=""
+    FMT_GREEN=""
+    FMT_YELLOW=""
+    FMT_BLUE=""
+    FMT_BOLD=""
+    FMT_RESET=""
+    return
+  fi
+
+  if supports_truecolor; then
+    FMT_RAINBOW="
+      $(printf '\033[38;2;255;0;0m')
+      $(printf '\033[38;2;255;97;0m')
+      $(printf '\033[38;2;247;255;0m')
+      $(printf '\033[38;2;0;255;30m')
+      $(printf '\033[38;2;77;0;255m')
+      $(printf '\033[38;2;168;0;255m')
+      $(printf '\033[38;2;245;0;172m')
+    "
+  else
+    FMT_RAINBOW="
+      $(printf '\033[38;5;196m')
+      $(printf '\033[38;5;202m')
+      $(printf '\033[38;5;226m')
+      $(printf '\033[38;5;082m')
+      $(printf '\033[38;5;021m')
+      $(printf '\033[38;5;093m')
+      $(printf '\033[38;5;163m')
+    "
+  fi
+
+  FMT_RED=$(printf '\033[31m')
+  FMT_GREEN=$(printf '\033[32m')
+  FMT_YELLOW=$(printf '\033[33m')
+  FMT_BLUE=$(printf '\033[34m')
+  FMT_BOLD=$(printf '\033[1m')
+  FMT_RESET=$(printf '\033[0m')
+}
+
+check_alias() {
+  grep "~/.vim/config/alias" $1
+}
+
+set_command() {
+  cat >> $1 <<- EOF
 source ~/.vim/config/alias
 [[ -s ~/.autojump/etc/profile.d/autojump.sh ]] && source ~/.autojump/etc/profile.d/autojump.sh
 autoload -U compinit && compinit -u
 EOF
-  }
+}
 
+set_shell() {
   shell_name=~/.bashrc
   if [ -f ~/.zshrc ]; then
     shell_name=~/.zshrc
@@ -26,68 +98,113 @@ EOF
   fi
 }
 
-#install brew for Mac
-if [ "$(uname)" == "Darwin" ];then
-  # if brew not exist then install it
-  if ! $(command -v brew > /dev/null); then
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-  fi
-fi
-
-#main
-#start config vim and alias
-#
-if which apt-get >/dev/null 2>&1; then
-  apt-get install -y universal-ctags ack cscope silversearcher-ag git zsh
-elif which yum >/dev/null 2>&1; then
-  yum install -y ctags ack cscope the_silver_searcher git zsh
-elif which brew >/dev/null 2>&1;then
-  brew install ctags ack cscope tmux the_silver_searcher gsed git zsh
-fi
-
-if [ ! -f /usr/local/bin/ctags ];then
-  ln -s /usr/bin/ctags /usr/local/bin/ctags
-fi
-
-#exist ivim do't backup and git clone
-if [ ! -f ~/.vim/setup.sh ];then
-  #backup old_vim
-  if [ -d ~/.vim ]; then
-    mv -f ~/.vim ~/vim_old
+install_apps() {
+  #install brew for Mac
+  if [ "$(uname)" == "Darwin" ];then
+    # if brew not exist then install it
+    if ! $(command -v brew > /dev/null); then
+      sh -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    fi
   fi
 
-  if [ -f ~/.vimrc ];then
-    mv -f ~/.vimrc ~/.vimrc_old
+# install apps
+  if which apt-get >/dev/null 2>&1; then
+    apt-get install -y universal-ctags ack cscope silversearcher-ag git zsh
+  elif which yum >/dev/null 2>&1; then
+    yum install -y ctags ack cscope the_silver_searcher git zsh
+  elif which brew >/dev/null 2>&1;then
+    brew install ctags ack cscope tmux the_silver_searcher gsed git zsh
   fi
 
-  #ivim config
-  cd ~/ && git clone https://github.com/lostedleo/ivim.git ~/.vim
-fi
+  if [ ! -f /usr/local/bin/ctags ];then
+    ln -s /usr/bin/ctags /usr/local/bin/ctags
+  fi
+}
+
+backup_vim() {
+  #exist ivim do't backup and git clone
+  if [ ! -f ~/.vim/setup.sh ];then
+    #backup old_vim
+    if [ -d ~/.vim ]; then
+      mv -f ~/.vim ~/vim_old
+    fi
+
+    if [ -f ~/.vimrc ];then
+      mv -f ~/.vimrc ~/.vimrc_old
+    fi
+
+    #ivim config
+    cd ~/ && git clone https://github.com/lostedleo/ivim.git ~/.vim
+  fi
+}
 
 #install efficient tools
+
 #install ohmyzsh
-sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-#install omz plugins
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-sed -i.bak 's/plugins=(git)/plugins=(git zsh-autosuggestions zsh-syntax-highlighting)/' ~/.zshrc
+install_omz() {
+  if [ -d ~/.oh-my-zsh ];then
+    echo "${FMT_YELLOW}Found oh-my-zsh, not to install oh-my-zsh${FMT_RESET}"
+    return
+  fi
+
+  mkdir -p ~/.temp
+  wget -O ~/.temp/omz_install.sh https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh
+
+  /usr/bin/expect << EOF
+  spawn bash ~/.temp/omz_install.sh
+  expect {
+    "Y/n" { send "y\n" }
+  }
+  expect eof
+EOF
+
+  rm ~/.temp/omz_install.sh
+  #install omz plugins
+  git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+  git clone https://github.com/zsh-users/zsh-syntax-highlighting ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+  sed -i.bak 's/plugins=(git)/plugins=(git zsh-autosuggestions zsh-syntax-highlighting)/' ~/.zshrc
+
+  echo "${FMT_GREEN}Install oh-my-zsh success!${FMT_RESET}"
+}
 
 #install autojump
-git clone https://github.com/wting/autojump.git ~/.temp/autojump
-cd ~/.temp/autojump && ./install.py && cd -
+install_autojump() {
+  if [ -d ~/.fzf ]; then
+    echo "${FMT_YELLOW}Found autojump, not to install!${FMT_RESET}"
+    return
+  fi
+  git clone https://github.com/wting/autojump.git ~/.temp/autojump
+  cd ~/.temp/autojump && ./install.py && cd -
+  echo "${FMT_GREEN}Install autojump success!${FMT_RESET}"
+}
 
-#set shell config
-set_shell
-#set vim config
-ln -s ~/.vim/vimrc ~/.vimrc
-ln -s ~/.vim/config/screenrc ~/.screenrc
-ln -s ~/.vim/config/tmux.conf ~/.tmux.conf
-mkdir -p ~/.ssh/control
-ln -s ~/.vim/config/ssh_config ~/.ssh/config
+#set link config
+set_link() {
+  ln -s ~/.vim/vimrc ~/.vimrc
+  ln -s ~/.vim/config/screenrc ~/.screenrc
+  ln -s ~/.vim/config/tmux.conf ~/.tmux.conf
+  mkdir -p ~/.ssh/control
+  ln -s ~/.vim/config/ssh_config ~/.ssh/config
+}
+
+install_fzf() {
+  if [ -d ~/.fzf ];then
+    echo "${FMT_YELLOW}Found fzf, not to install!${FMT_RESET}"
+    return
+  fi
+
+  /usr/bin/expect << EOF
+  spawn ~/.vim/bundle/fzf/install
+  expect {
+    "/n" { send "y\r"; exp_continue}
+  }
+EOF
+}
 
 #install all vim plugins
-if [ ! -d ~/.vim/bundle ];then
-  git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+install_vim_plugins() {
+  if [ ! -d ~/.vim/bundle ];then
+    git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 
   #install vim plugin
   tmp_file=~/.vim/ivim_temp
@@ -96,6 +213,41 @@ if [ ! -d ~/.vim/bundle ];then
   echo "please wating..." >> $tmp_file
   vim $tmp_file -c "BundleInstall" -c "q" -c "q"
   rm $tmp_file
-  ~/.vim/bundle/fzf/install
-  echo "install completed!"
-fi
+
+  install_fzf
+  echo "${FMT_GREEN}install vim plugins and fzf!${FMT_RESET}"
+  fi
+}
+
+source_shell() {
+  shell_name=~/.bashrc
+  if [ -f ~/.zshrc ]; then
+    shell_name=~/.zshrc
+  fi
+  source ${shell_name}
+}
+
+install_apps_and_tools() {
+  install_apps
+  install_omz
+  install_autojump
+}
+
+set_config() {
+  set_link
+  set_shell
+}
+
+main() {
+  setup_color
+  # first install apps and tools
+  install_apps_and_tools
+  # second backup old vim and vimrc
+  backup_vim
+  # then install vim plugins
+  install_vim_plugins
+
+  echo "${FMT_GREEN}Success install ivim!${FMT_RESET}"
+}
+
+main
